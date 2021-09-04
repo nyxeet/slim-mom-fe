f39666293139c184eb76fc78f31c1f174ade8c41
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-09-04 16:06:15 +0300

    added reg page with logic

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import MainPage from './pages/MainPage';
+// import MainPage from './pages/MainPage';
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
-  return <MainPage />;
+  // return <MainPage />;
+  return <RegistrationPage />;
+  // return <LoginPage />;
 }
 
 export default App;
