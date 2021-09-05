33055f5a87c41e37a7b986914f9e243b70cc9a7d
Roman-Siratsky <55003770+Roman-Siratsky@users.noreply.github.com>
2021-09-05 19:32:07 +0300

    Revert "Feature/header tools"

--- src/redux/dailyRate/dailyRateSelectors.js
+++
@@ -1,5 +0,0 @@
-const getCalories = state => state.dailyRate.dailyRate;
-const getProducts = state => state.dailyRate.notAllowedProducts;
-
-// eslint-disable-next-line
-export default { getCalories, getProducts };
