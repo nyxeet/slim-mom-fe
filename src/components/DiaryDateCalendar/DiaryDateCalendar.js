eb55c56d7a41268ed290d20d7d86c2d290357246
OleksandrPenskyi <suchgoodidea@gmail.com>
2021-09-05 10:48:13 +0300

    added calendar

--- src/components/DiaryDateCalendar/DiaryDateCalendar.js
+++ src/components/DiaryDateCalendar/DiaryDateCalendar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import styles from './DiaryDateCalendar.module.css';
@@ -13,7 +13,7 @@ export default function DiaryDateCalendar() {
   const closeCalendar = () => setIsVisibe(false);
 
   return (
-    <>
+    <section className={styles.Wrapper}>
       <DatePicker
         selected={date}
         onChange={changeDate}
@@ -26,7 +26,17 @@ export default function DiaryDateCalendar() {
         open={isVisible}
         locale={ru}
       />
-      <div onClick={openCalendar} className={styles.Calendar}></div>
-    </>
+      <div onClick={openCalendar} className={styles.Calendar}>
+        <svg
+          className={styles.CalendarIcon}
+          width="20"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M15 9h-2v2h2V9zM11 9H9v2h2V9zM7 9H5v2h2V9z" />
+          <path d="M17 2h-1V0h-2v2H6V0H4v2H3c-1.11 0-1.99.9-1.99 2L1 18a2 2 0 002 2h14c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 16H3V7h14v11z" />
+        </svg>
+      </div>
+    </section>
   );
 }
