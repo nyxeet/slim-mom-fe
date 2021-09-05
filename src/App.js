3ff3bffb95bc7994104224191bf749c1b481b378
tetianako-prog <t.kobyliakova@gmail.com>
2021-09-05 12:25:08 +0300

    Add calculator page and right side bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
-// import MainPage from './pages/MainPage';
+import MainPage from './pages/MainPage';
 import RegistrationPage from './pages/RegistrationPage';
 import LoginPage from './pages/LoginPage';
+import CalculatorPage from './pages/CalculatorPage';
 
 function App() {
-  // return <MainPage />;
-  return <RegistrationPage />;
+  //return <MainPage />;
+  //return <RegistrationPage />;
   // return <LoginPage />;
+  return <CalculatorPage />;
 }
 
 export default App;
