f280693c4e1c500e6ff464ba7ca2ccc67bfe3305
tetianako-prog <t.kobyliakova@gmail.com>
2021-09-06 21:34:50 +0300

    header layout fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import MainPage from './pages/MainPage';
 import RegistrationPage from './pages/RegistrationPage';
+import { Switch, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import CalculatorPage from './pages/CalculatorPage';
 import DiaryDateCalendar from './components/DiaryDateCalendar/DiaryDateCalendar';
 import Header from './components/Header';
 
 function App() {
-  return <MainPage />;
+  return (
+    <>
+      <Header />
+      <MainPage />
+    </>
+  );
+
   // return <RegistrationPage />;
   // return <LoginPage />;
   //return <CalculatorPage />;
   // return <DiaryDateCalendar />;
   // return <Header/>;
+  //return <MainPage />;
 }
 
 export default App;
