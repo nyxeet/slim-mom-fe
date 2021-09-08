4257ec3e13fe973009bcc93ce537cb3d2a27e572
Ann Maksay <annaorlova.dn@gmail.com>
2021-09-08 15:38:27 +0300

    add routes

--- src/App.js
+++ src/App.js
@@ -7,11 +7,48 @@ import CalculatorPage from './pages/CalculatorPage';
 import DiaryDateCalendar from './components/DiaryDateCalendar/DiaryDateCalendar';
 import Header from './components/Header';
 
+import {
+  loginLink,
+  registrationLink,
+  homeLink,
+  diaryLink,
+  calculatorLink,
+} from './routes';
+import { PublicRoute, PrivateRoute } from './components/CustomRoutes';
+
 function App() {
   return (
     <>
       <Header />
-      <MainPage />
+      {/* // todo: Добавить Suspense,лоадер и ленивую загрузку по желанию или удалить закоментрованную часть*/}
+      {/* <Suspense fallback={<h1>Loading..</h1>}> */}
+      <Switch>
+        <PublicRoute
+          exact
+          path={homeLink}
+          restricted
+          redirectTo={calculatorLink}
+        >
+          <MainPage />
+        </PublicRoute>
+
+        <PublicRoute path={registrationLink} restricted redirectTo={diaryLink}>
+          <RegistrationPage />
+        </PublicRoute>
+
+        <PublicRoute path={loginLink} restricted redirectTo={diaryLink}>
+          <LoginPage />
+        </PublicRoute>
+
+        <PrivateRoute path={diaryLink} redirectTo={loginLink}>
+          <DiaryDateCalendar />
+        </PrivateRoute>
+
+        <PrivateRoute path={calculatorLink} redirectTo={loginLink}>
+          <CalculatorPage />
+        </PrivateRoute>
+      </Switch>
+      {/* </Suspense> */}
     </>
   );
 
--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,9 @@
 export const loginLink = '/login';
+
+export const registrationLink = '/registration';
+
+export const homeLink = '/';
+
+export const diaryLink = '/diary';
+
+export const calculatorLink = '/calculator';
